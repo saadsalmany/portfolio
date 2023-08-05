d635a302f567188431aea3c9f07ee22de0ca0dd9
Saad <mohdsaad88842@gmail.com>
2023-08-05 13:22:50 +0530

    improved

---
+++ index.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Web developer', 'UI/UX designer','Web designer','Graphic designer'],
+    typeSpeed: 50, startDelay: 4200, afterDelay: 5000
+  });
