ac1b74b15b5c87275f4988d8e0ef78e726e1f268
Saad Salmany <mohdsaad88842@gmail.com>
2023-09-17 20:37:41 +0530

    updated

--- index.js
+++
@@ -1,4 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ['Web developer', 'UI/UX designer','Web designer','Graphic designer'],
-    typeSpeed: 50, startDelay: 4200, afterDelay: 5000
-  });
