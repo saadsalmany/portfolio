464e25aa35af2f648624cd2f5a28f3c4343cfee6
saadsalmany <mohdsaad88842@gmail.com>
2023-11-03 16:46:28 +0530

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+//Typed js library
+
+var typed = new Typed(".autoType", {
+    strings: ['UI/UX Designer', 'Android Developer', 'Web Developer'],
+    typeSpeed: 80, startDelay: 6000
+  });
+
+  // Disable scrolling by setting overflow to "hidden"
+document.body.style.overflow = 'hidden';
+document.documentElement.style.overflow = 'hidden';
+
+// Re-enable scrolling after 4 seconds
+setTimeout(function() {
+  document.body.style.overflow = 'auto';  // or 'scroll' to allow scrolling
+  document.documentElement.style.overflow = 'auto';  // or 'scroll' to allow scrolling
+}, 4000); // 4 seconds in milliseconds (4,000 milliseconds)
